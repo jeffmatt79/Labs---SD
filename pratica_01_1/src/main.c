3d142bee2842dbd02faf8dacffa4963ce5375f99
jeffmatt79 <112177714+jeffmatt79@users.noreply.github.com>
2025-07-13 00:23:59 -0300

    Add files via upload

---
+++ pratica_01_1/src/main.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  26-05-2015 17:31:15
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Francisco Helder (), helderhdw@gmail.com 
+ *   Organization:  Federal University of Ceara
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main (){
+
+	printf("Hello World!!!");
+
+	return(0);
+}
