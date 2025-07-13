5488074d3da46d9a945172dc2baed57575640196
jeffmatt79 <112177714+jeffmatt79@users.noreply.github.com>
2025-07-13 00:23:03 -0300

    Add files via upload

---
+++ ejb-lab/ejb-demo/src/main/java/com/ejb/HelloBean.java
@@ -0,0 +1,11 @@
+package com.ejb;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class HelloBean {
+    public String sayHello(String name) {
+        return "Olá " + name + ", este é um EJB!";
+    }
+}
+
